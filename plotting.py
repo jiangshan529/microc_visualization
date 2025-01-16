416ae34eb65cf2fc007885dcf3de3f73adb3ed29
jiangshan529 <75197626+jiangshan529@users.noreply.github.com>
2025-01-15 20:37:32 -0500

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,60 +1,58 @@
-### fig1, 5kb
-mcool_files =["/broad/boxialab/shawn/projects/microC/merged_new/remake_UU/HEK_WT/HEK_merge_micro1_5_UU_merged_dedup.pairs_652M_addnorm_200.mcool"]
-chip_condition_bigwigs = ["/broad/boxialab/shawn/projects/microC/merged_new/remake_UU/HEK_WT/HEK_merge_micro1_5_UU_merged_dedup.pairs_652M_addnorm_5000_ins.bedgraph_5k.125000.bw",
-       "/broad/boxialab/shawn/projects/hic/chip-seq/2.peak/HEK293_J_HG38_SRR1015994.bigwig",
-                          "/broad/boxialab/shawn/projects/pub_data/chip-seq/hek_cpm_chip/H7_2min_6min_merge_sort_dedup_CPM.bw",
-                          "/broad/boxialab/rawdata/8th_batch_1004_shawnbai/flag_chip/2.track_peak/227CVTLT3_5_0420637077_ChIP-80_S39_L005_hg38_sort_cpm.rmdup.bw",
-                          "/broad/boxialab/rawdata/8th_batch_1004_shawnbai/flag_chip/2.track_peak/227CVTLT3_1_0420636994_ChIP-79_S6_L001_hg38_sort_cpm.rmdup.bw",
-    
-                          "/broad/boxialab/shawn/projects/pub_data/chip-seq/hek_cpm_chip/23022FL-03-01-27_S27_L001_hg38_sort_cpm.rmdup.bw",
-                          "/broad/boxialab/shawn/projects/pub_data/chip-seq/hek_cpm_chip/23022FL-03-01-28_S28_L001_hg38_sort_cpm.rmdup.bw",
-                          
-                          "/broad/boxialab/shawn/projects/analysis/chip_Umap/bw_files/1.fastq/2.track_peak/SRR1016003_hg38_single_aligned_sort_dedup.bigwig"
-                          
-                           ]
-condition_order = ["insulation","publicJ","H7J","flag","igG","ctcf","rad21","h3k27ac"]
-DEFAULT_COLOR_LIST = ["red","#6e750e","#1fa774","#1fa774","#929591","#0485d1","#fd3c06","#960056"]
-ymax=[1,0.15,0.3,0.3,0.3,2,2,2]
-ymin=[-1,0,0,0,0,0,0,0]
-# condition_order = ['ins', 'abcam', 'H7',"public_J","CTCF"]
-# DEFAULT_COLOR_LIST = ["#1fa774","#1fa774", "#1fa774","#0485d1"]
-# ymax=[1,1,1,1,20]
-# ymin=[0,0,0,0,0]
-# chip_condition_bigwigs = ["/broad/boxialab/shawn/projects/microC/bw_track/HCT/HCT116_J_chip_85_87_sort_dedup.bw",
-#                           "/broad/boxialab/shawn/projects/microC/bw_track/HCT/227CVTLT3_2_0420637073_ChIP-55_S10_L002_hg38_sort.rmdup.bw",
-#                             "/broad/boxialab/shawn/projects/microC/bw_track/HCT/HCT116_POL2A_hg38_ENCFF779RGR.bigWig",
-#                          "/broad/boxialab/shawn/projects/microC/bw_track/HCT/HCT116_H3K27ac_ENCFF758DHJ.bigWig",
-#                          "/broad/boxialab/shawn/projects/J/self_data/RNA-seq/HCT116/23022FL-07-01-04_S4_L008Aligned.bw"]
+
+
+# 数据配置
+mcool_files = ["/broad/boxialab/shawn/projects/microC/merge_publication_1.7B/hct/hct_wt_merge_all_1.48B_UU_dedup.pairs_50bp.mcool"]
+chip_condition_bigwigs = [
+    "/broad/boxialab/shawn/projects/microC/merge_publication_1.7B/hct/hct_wt_ins_125000_5k_rm3.125000.bw",
+    "/broad/boxialab/shawn/projects/microC/merge_publication_1.7B/hek/hek_wt_1.7b_win25_rm3.125000.bw",
+    "/broad/boxialab/shawn/projects/microC/merge_publication_1.7B/hct/hct_wt_ins_200res_5000_rm3.5000.bw",
+    "/broad/boxialab/shawn/projects/microC/merge_publication_1.7B/hek/hct_wt_ins_200bp_5000_rm3.5000.bw",
+    "/broad/boxialab/shawn/projects/analysis/chip_Umap/bw_files/1.fastq/2.track_peak/H7_2min_6min_merge_sort_dedup_CPM.bigwig",
+    "/broad/boxialab/shawn/projects/analysis/chip_Umap/bw_files/HCT/fastq/2.track_peak/HCT116_J_chip_85_87_sort_dedup.bw",
+    "/broad/boxialab/shawn/projects/analysis/chip_Umap/bw_files/1.fastq/2.track_peak/hek_znf_merge_sort_dedup.bigwig",
+    "/broad/boxialab/shawn/projects/analysis/chip_Umap/bw_files/1.fastq/2.track_peak/23022FL-03-01-27_S27_L001_hg38_sort_cpm.rmdup.bigwig",
+    "/broad/boxialab/shawn/projects/analysis/chip_Umap/bw_files/HCT/fastq/2.track_peak/227CVTLT3_2_0420637073_ChIP-55_S10_L002_hg38_sort.rmdup.bw",
+    "/broad/boxialab/shawn/projects/analysis/chip_Umap/bw_files/1.fastq/2.track_peak/23022FL-03-01-28_S28_L001_hg38_sort_cpm.rmdup.bigwig",
+    "/broad/boxialab/shawn/projects/analysis/chip_Umap/bw_files/HCT/fastq/2.track_peak/227CVTLT3_3_0420637074_ChIP-56_S20_L003_hg38_sort.rmdup.bw",
+    "/broad/boxialab/shawn/projects/analysis/chip_Umap/bw_files/1.fastq/2.track_peak/SRR1016003_hg38_single_aligned_sort_dedup.bigwig",
+    "/broad/boxialab/shawn/projects/analysis/chip_Umap/bw_files/HCT/fastq/2.track_peak/hct_h3k27ac_ENCFF623IJZ_hg38_sort_cpm.rmdup.bw",
+    "/broad/boxialab/shawn/projects/pub_analysis/flag_ko_chip/227CVTLT3_1_0420636994_ChIP-79_S6_L001_hg38_sort_cpm.rmdup.bw"
+]
+
+# 添加两个 BED 文件
+bed_files = [
+    "/broad/boxialab/shawn/projects/analysis/ctcf_motif/hek_CTCF_motifs_fpr0.1.bed",
+    "/broad/boxialab/shawn/projects/analysis/ctcf_motif/hct_CTCF_motifs_fpr0.1.bed"
+]
+
+condition_order = ["insulation", "insulation", "insulation", "insulation", "hek_H7J", "hct_H7J", "hek_znf", "hek_ctcf", "hct_ctcf", "hek_rad21", "hct_rad21", "hek_27ac", "hct_27ac", "hek_igg"]
+DEFAULT_COLOR_LIST = ["red", "red", "red", "red", "#1fa774", "#1fa774", "#0014A8", "#0485d1", "#0485d1", "#fd3c06", "#fd3c06", "#960056", "#960056", "grey"]
+ymax = [1, 1, 1, 1, 0.5, 0.5, 2, 5, 5, 3, 3, 2, 2, 1]
+ymin = [-1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 proseq_path = "/broad/boxialab/shawn/public_data/PRO-seq/HCT" # provide path to proseq files
 proseq_forward_bigwigs = ["HCT116-PRO_merge_hg38_plus_sort.bw","HCT116-PRO_merge_hg38_plus_sort.bw"]
 proseq_reverse_bigwigs = ["HCT116-PRO_merge_hg38_minus_sort.bw","HCT116-PRO_merge_hg38_plus_sort.bw"]
 
-# # forward_proseq_list = ["/broad/boxialab/shawn/public_data/PRO-seq/HCT/HCT116-PRO_merge_hg38_plus_sort.bw",
-#                       "/broad/boxialab/shawn/public_data/PRO-seq/HCT/HCT116-PRO_merge_hg38_plus_sort.bw"]
-# reverse_proseq_list = ["/broad/boxialab/shawn/public_data/PRO-seq/HCT/HCT116-PRO_merge_hg38_minus_sort.bw",
-#                       "/broad/boxialab/shawn/public_data/PRO-seq/HCT/HCT116-PRO_merge_hg38_plus_sort.bw"]
 
 
-
-# chip_condition_bigwigs = cat_prefix_to_list(chip_path, chip_condition_bigwigs)
-# proseq_forward_bigwigs = cat_prefix_to_list(proseq_path, proseq_forward_bigwigs)
-# proseq_reverse_bigwigs = cat_prefix_to_list(proseq_path, proseq_reverse_bigwigs)
-
-# condition_order = ['h7j', 'CTCF', 'polII',"27AC","RNA"]
-# DEFAULT_COLOR_LIST = ["#1fa774", "#0485d1","#8e82fe","#960056","#fd3c06"]
-# ymax=[0.7,5,10,70,70]
-# ymin=[0,0,0,0,0]
 bw_bins=7000
 track_height=0.5
-region_str = "chr8:101,199,193-103,133,192" #ovide region in format like chr15:99,921,434-100,682,822
+
+
+region_str = ("19:27,638,001-30,000,000")
+
+chrom, positions = region_str.split(":")
+start, end = map(int, positions.replace(",", "").split("-"))
+region_gr = GenomeRange(f"chr{chrom}", start, end)  
 
 MICROC_PLOTTING_PARAMS = {"style":"box",
                           "depth_ratio":"full",
                           "balance":True,
-                          "cmap":'JuiceBoxLike',
-                          "max_value":-4}
+                          "cmap":'fall',
+                          "max_value":-6,
+                         "min_value":-8}
 
-region_plot_params = {"region": region_str,
+region_plot_params = {"region": region_gr,
                     "resolution": 5000,
                     "highlight_region_list": [["chr1",154_955_735,154_994_524]], 
                     "microc_file": mcool_files[0],
@@ -62,7 +60,8 @@ region_plot_params = {"region": region_str,
                     "chip_list_list": [chip_condition_bigwigs],
                     "forward_proseq_list": proseq_forward_bigwigs,
                     "reverse_proseq_list": proseq_reverse_bigwigs,
-                     "condition_order": condition_order}
+                     "condition_order": condition_order,
+                     "bed_files":bed_files}
 
 frame = make_region_plot(**region_plot_params)
 fig1 = frame.plot(region_str)
